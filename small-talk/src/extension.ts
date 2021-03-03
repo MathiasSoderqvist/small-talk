3a918abc0e2d58779ac308e87bf960bf407e9238
Mathias Soderqvist <mathias.soderqvist@live.com>
2021-03-03 19:39:51 +0100

    add status bar item

--- small-talk/src/extension.ts
+++ small-talk/src/extension.ts
@@ -6,23 +6,30 @@ let myStatusBarItem: vscode.StatusBarItem;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate({subscriptions}: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "small-talk" is now active!');
-
-	// The command has been defined in the package.json file
+	const myCommandId = 'small-talk.statusBarClick';
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('small-talk.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+	
+	//show button on vs code load
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Small Talk!');
-	});
+		vscode.window.showInformationMessage('Ready to translate :)');
 
-	context.subscriptions.push(disposable);
+		updateStatusBarItem();
+	}));
+}
+
+//status bar default language translate
+function updateStatusBarItem(): void {
+	myStatusBarItem.text = `$(word-wrap) translate`;
+	
+	myStatusBarItem.show();
 }
 
 // this method is called when your extension is deactivated
